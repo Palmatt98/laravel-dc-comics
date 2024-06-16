25b4d0fd0ef4a1257efe9a0cbe4d2a268755ca78
Palmatt98 <palmierimatteo98@gmail.com>
2024-06-17 00:18:31 +0200

    add: migrate seeder

---
+++ database/migrations/2024_06_16_213039_create_dc_comics_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dc_comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('thumb');
+            $table->decimal('price', 2, 2);
+            $table->date('sale_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dc_comics');
+    }
+};
---
+++ database/seeders/DcComicsTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DcComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = config('dc_comics');
+        foreach
+    }
+}
